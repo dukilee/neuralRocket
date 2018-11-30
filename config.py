42f79817384c720901d5490df35e54aec949293f
Guilherme Oliveira <guilherme.oliveira@tfgco.com>
2018-11-30 12:18:33 -0200

    Adding config file

---
+++ config.py
@@ -0,0 +1,36 @@
+import math
+
+game = {
+	'caption':'Neural Rocket',
+	'height' : 600,
+	'width'  : 800,
+	'fps'    : 30,
+	'scale'  : 50, #conversion from meters to pixels
+};
+
+initialPosition = {
+	#(0, 0) the center of the floor
+	'x':0, #in meters
+	'y':10, #in meters
+	'angle':math.pi/2, #in radians
+};
+
+rocket = {
+	'mass'        :10,   #in kg
+	'width'       :1,    #in meters
+	'height'      :4,    #in meters
+	'motorPower'  :100,  #in newtons
+};
+
+physics = {
+	'gravity'   :9.81,   #in m/s²
+};
+
+colors = {
+	'white':(255, 255, 255),
+	'black':(0, 0, 0),
+	'red':(200, 0, 0),
+	'green':(0, 200, 0),
+	'blue':(0, 0, 200),
+
+};
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
+import config
 
-display = pygame.display.set_mode((800, 600));
-pygame.display.set_caption('Anaconda');
+print(config.game);
 
+display = pygame.display.set_mode((config.game['width'], config.game['height']));
+pygame.display.set_caption(config.game['caption']);
 clock = pygame.time.Clock();
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit();
 
-		pygame.display.update();
-		clock.tick(30);
+	pygame.display.update();
+	clock.tick(config.game['fps']);
